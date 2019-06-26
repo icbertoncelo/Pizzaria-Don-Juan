5b5d84b5111d64e52a9176cb066af8676d752f77
Ian Carlos <carlos.ian007@gmail.com>
2019-06-26 20:05:18 -0300

    Feat(redux): Add redux and reactotron

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import '~/config/reactotron';
 
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import store from '~/store';
 import Routes from '~/routes/index';
 import GlobalStyle from '~/styles/global';
 
 const App = () => (
-  <>
-    <GlobalStyle />
-    <Routes />
-  </>
+  <Provider store={store}>
+    <>
+      <GlobalStyle />
+      <Routes />
+    </>
+  </Provider>
 );
 
 export default App;
