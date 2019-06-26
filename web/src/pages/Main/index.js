9ee6cfa6dfb1467fe205d958c6341e7585ef020c
Ian Carlos <carlos.ian007@gmail.com>
2019-06-25 23:14:54 -0300

    Style(main): Add main component

--- web/src/pages/Main/index.js
+++ web/src/pages/Main/index.js
@@ -1,11 +1,73 @@
 import React from 'react';
 
-import { Container } from './styles';
+import {
+  Header,
+  LogoContainer,
+  UserDataContainer,
+  Content,
+  Order,
+  OrderHeader,
+  ItemsContainer,
+  Item,
+} from './styles';
+import Logo from '~/assets/logo.png';
 
 const Main = () => (
-  <Container>
-    <h1>Main</h1>
-  </Container>
+  <>
+    <Header>
+      <LogoContainer>
+        <img src={Logo} alt="Logo" />
+        <strong>Pizzaria Don Juan</strong>
+      </LogoContainer>
+      <UserDataContainer>
+        <strong>Ian Carlos</strong>
+        <button type="button">Sair</button>
+      </UserDataContainer>
+    </Header>
+
+    <Content>
+      <h1>Últimos Pedidos</h1>
+      <Order>
+        <OrderHeader>
+          <h2>Pedido #3 - Ian carlos</h2>
+          <span>há 2 horas</span>
+          <strong>R$36,00</strong>
+        </OrderHeader>
+        <ItemsContainer>
+          <Item>
+            <img
+              src="https://img.pngio.com/free-png-pepperoni-pizza-png-images-transparent-free-png-images-pizza-850_834.png"
+              alt="Order image"
+            />
+            <div>
+              <strong>Pizza Portuguesa</strong>
+              <span>Tamanho: M</span>
+            </div>
+          </Item>
+          <Item>
+            <img
+              src="https://img.pngio.com/free-png-pepperoni-pizza-png-images-transparent-free-png-images-pizza-850_834.png"
+              alt="Order image"
+            />
+            <div>
+              <strong>Pizza Portuguesa</strong>
+              <span>Tamanho: M</span>
+            </div>
+          </Item>
+          <Item>
+            <img
+              src="https://img.pngio.com/free-png-pepperoni-pizza-png-images-transparent-free-png-images-pizza-850_834.png"
+              alt="Order image"
+            />
+            <div>
+              <strong>Pizza Portuguesa</strong>
+              <span>Tamanho: M</span>
+            </div>
+          </Item>
+        </ItemsContainer>
+      </Order>
+    </Content>
+  </>
 );
 
 export default Main;
--- web/src/pages/Main/styles.js
+++ web/src/pages/Main/styles.js
@@ -1,3 +1,124 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 80px;
+  justify-content: space-around;
+  background-color: #07162c;
+  color: #fff;
+`;
+
+export const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+
+  img {
+    height: 40px;
+    margin-right: 17px;
+  }
+
+  strong {
+    font-size: 18px;
+    font-weight: bold;
+  }
+`;
+
+export const UserDataContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0 10px;
+  border-right: 1px solid #999;
+
+  strong {
+    font-size: 18px;
+    font-weight: bold;
+    color: #fff;
+  }
+
+  button {
+    background: transparent;
+    border: 0;
+    color: #999;
+    text-align: right;
+
+    &:hover {
+      color: #fff;
+    }
+  }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 20px;
+
+  h1 {
+    font-size: 22px;
+    font-weight: bold;
+  }
+`;
+
+export const Order = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  padding: 10px;
+  margin-top: 20px;
+  border-radius: 7px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+
+  &:last-child {
+    margin-bottom: 20px;
+  }
+`;
+
+export const OrderHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  h2 {
+    font-size: 18px;
+  }
+
+  span {
+    font-size: 12px;
+    color: #999;
+    margin-top: 5px;
+  }
+
+  strong {
+    font-size: 14px;
+    margin-top: 5px;
+    font-weight: bold;
+  }
+`;
+
+export const ItemsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin-top: 20px;
+`;
+
+export const Item = styled.div`
+  display: flex;
+  width: 49%;
+  border: 1px solid #ddd;
+  border-radius: 7px;
+  padding: 5px;
+  margin-top: 10px;
+
+  img {
+    height: 100px;
+    width: 100px;
+    margin-right: 15px;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+`;
