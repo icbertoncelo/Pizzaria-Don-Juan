642d8a5d50e838f6ff11cd9041fe40fa3bc564b1
Ian Carlos <carlos.ian007@gmail.com>
2019-06-29 01:02:58 -0300

    Style(login): Add login page

--- PizzariaApp/src/pages/Login/index.js
+++ PizzariaApp/src/pages/Login/index.js
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import { View } from 'react-native';
+import { StatusBar } from 'react-native';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Transparency,
+  Form,
+  LogoImage,
+  Input,
+  AccessButton,
+  AccessText,
+} from './styles';
 
-const Login = () => <View />;
+import Logo from '~/assets/logo.png';
+import Fundo from '~/assets/fundo.jpg';
+
+class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  render() {
+    const { email, password } = this.state;
+    return (
+      <Container source={Fundo}>
+        <Transparency colors={['transparent', 'black']}>
+          <StatusBar hidden barStyle="light-content" />
+          <Form>
+            <LogoImage source={Logo} />
+            <Input
+              autoCapitalize="none"
+              autoCorrect={false}
+              placeholder="Seu e-mail"
+              underlineColorAndroid="transparent"
+              value={email}
+              onChangeText={text => this.setState({ email: text })}
+            />
+            <Input
+              autoCapitalize="none"
+              autoCorrect={false}
+              placeholder="Senha Secreta"
+              underlineColorAndroid="transparent"
+              value={password}
+              onChangeText={text => this.setState({ password: text })}
+            />
+            <AccessButton onPress={() => {}}>
+              <AccessText>Entrar</AccessText>
+            </AccessButton>
+          </Form>
+        </Transparency>
+      </Container>
+    );
+  }
+}
 
 export default Login;
---
+++ PizzariaApp/src/pages/Login/styles.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components/native';
+import LinearGradient from 'react-native-linear-gradient';
+
+export const Container = styled.ImageBackground`
+  flex: 1;
+`;
+
+export const Transparency = styled(LinearGradient)`
+  position: absolute;
+  align-items: center;
+  justify-content: center;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`;
+
+export const Form = styled.View`
+  position: absolute;
+  flex-direction: column;
+  align-items: center;
+  width: 300px;
+`;
+
+export const LogoImage = styled.Image`
+  width: 72px;
+  height: 72px;
+  margin-bottom: 25px;
+`;
+export const Input = styled.TextInput`
+  width: 100%;
+  height: 42px;
+  border: 0;
+  border-radius: 7px;
+  margin-bottom: 10px;
+  padding: 0 20px;
+  font-size: 16px;
+  background-color: #fff;
+  color: #8f8f8f;
+`;
+export const AccessButton = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.7,
+})`
+  width: 100%;
+  height: 42px;
+  border: 0;
+  border-radius: 7px;
+  background: #e64c4c;
+  justify-content: center;
+  align-items: center;
+`;
+export const AccessText = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+  color: #fff;
+`;
